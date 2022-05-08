8c5c36eb1be2ac871c4d03e8179b06cc140bcf0f
tgl-dogg <vinidoggll@hotmail.com>
2022-05-08 16:55:48 -0300

    Add gin dependencies and data mockup for GET request

--- heroes-microservice/main.go
+++ heroes-microservice/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// sample
+var races = []Race{
+	{ID: "1", Name: "Human", Description: "We all understand the concept of a human.", Strength: 3, Agility: 3, Intelligence: 3, Willpower: 3},
+	{ID: "2", Name: "Elf", Description: "Pointy ears and snob noses. Live in forests.", Strength: 2, Agility: 4, Intelligence: 3, Willpower: 3},
+	{ID: "3", Name: "Dwarf", Description: "Small and strong, like montains and steel.", Strength: 4, Agility: 2, Intelligence: 3, Willpower: 3},
+}
 
 func main() {
-	fmt.Println("I will be a microservice someday!")
+	router := gin.Default()
+	router.GET("/races", getRaces)
+
+	router.Run("localhost:8080")
+}
+
+func getRaces(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, races)
 }
