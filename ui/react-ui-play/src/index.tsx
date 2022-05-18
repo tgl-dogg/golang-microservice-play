3b9aa41980b257a234276a2cd08bf30f439fa244
tgl-dogg <vinidoggll@hotmail.com>
2022-05-17 23:34:08 -0300

    Downgrade to React 17 because of related issue with custom components.
    Related issue: https://github.com/facebook/react/issues/24304

--- ui/react-ui-play/src/index.tsx
+++ ui/react-ui-play/src/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+      <App />
+  </React.StrictMode>,
+  document.getElementById("root"),
 );
 
 // If you want your app to work offline and load faster, you can change
